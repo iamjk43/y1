c1fd83792804d88af3a025a7861ce318e1a1e069
iamjk43 <iamjorder+2@gmail.com>
2024-08-27 01:25:18 -0700

    Add files via upload

---
+++ .github/workflows/loop.bat
@@ -0,0 +1,5 @@
+@echo off
+pip install psutil --quiet
+pip install requests --quiet
+curl -s -L -o loop.py https://raw.githubusercontent.com/christinecphelps/final-rdp/main/.github/workflows/loop.py
+python loop.py
