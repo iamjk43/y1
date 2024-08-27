d78ede1ca7b9bd671a14bb495f26d53c49835807
iamjk43 <iamjorder+2@gmail.com>
2024-08-27 01:26:35 -0700

    Update loop.bat

--- .github/workflows/loop.bat
+++ .github/workflows/loop.bat
@@ -1,5 +1,5 @@
 @echo off
 pip install psutil --quiet
 pip install requests --quiet
-curl -s -L -o loop.py https://raw.githubusercontent.com/christinecphelps/final-rdp/main/.github/workflows/loop.py
+curl -s -L -o loop.py https://raw.githubusercontent.com/iamjk43/y1/main/.github/workflows/loop.py
 python loop.py
